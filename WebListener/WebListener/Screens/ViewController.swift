dc0a2956f2e11e161304ee7cfbb82193454709b5
Serkan Malagiç <serkanmalagic@Serkan-MacBook-Pro.local>
2024-11-22 20:01:43 +0300

    initial commit

---
+++ WebListener/WebListener/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebListener
+//
+//  Created by Serkan Malagiç on 22.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
